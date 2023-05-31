1f6551c061b3d42263304c06ca2ce8ba261aa857
Иван Чернов <115635989+IvanChernov04@users.noreply.github.com>
2023-05-31 20:24:17 +0300

    Add files via upload

--- B31.cpp
+++ B31.cpp
@@ -1,15 +1,16 @@
-//самостоятельная базовая 1, B31
+// сам раб 2 баз B31
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    float fahrenheit;
-    cout << "Введите значение температуры в градусах Фаренгейта: ";
-    cin >> fahrenheit;
-
-    float celsius = (fahrenheit - 32) * 5 / 9; 
-    cout << "Значение температуры в градусах Цельсия: " << celsius << endl;
-
+    int a, b, c;
+    cout << "Enter the sides of the triangle: ";
+    cin >> a >> b >> c;
+    if (a == b || a == c || b == c) {
+        cout << "The triangle is isosceles." << endl;
+    } else {
+        cout << "The triangle is not isosceles." << endl;
+    }
     return 0;
 }
